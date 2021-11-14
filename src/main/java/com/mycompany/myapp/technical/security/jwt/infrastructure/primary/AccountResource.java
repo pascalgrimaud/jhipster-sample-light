c05e9b358e598c74b83c7e46b5933b81b4a0f614
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-11-14 20:10:47 +0100

    add security (wip)

---
+++ src/main/java/com/mycompany/myapp/technical/security/jwt/infrastructure/primary/AccountResource.java
@@ -0,0 +1,22 @@
+package com.mycompany.myapp.technical.security.jwt.infrastructure.primary;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api")
+public class AccountResource {
+
+  private final Logger log = LoggerFactory.getLogger(AccountResource.class);
+
+  @GetMapping("/authenticate")
+  public String isAuthenticated(HttpServletRequest request) {
+    log.debug("REST request to check if the current user is authenticated");
+    return request.getRemoteUser();
+  }
+}
