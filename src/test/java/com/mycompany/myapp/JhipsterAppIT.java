a65585ef2e66fbc18776131c6d5186ea8b1d5efa
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-11-27 09:32:23 +0100

    add spring boot

---
+++ src/test/java/com/mycompany/myapp/JhipsterAppIT.java
@@ -0,0 +1,65 @@
+package com.mycompany.myapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+@IntegrationTest
+class JhipsterAppIT {
+
+  @Test
+  void shouldMain() {
+    assertThatCode(() -> JhipsterApp.main(new String[] {})).doesNotThrowAnyException();
+  }
+
+  @Test
+  void shouldGetProtocol() {
+    assertThat(JhipsterApp.getProtocol(null)).isEqualTo("http");
+  }
+
+  @Test
+  void shouldGetProtocolForBlank() {
+    assertThat(JhipsterApp.getProtocol(" ")).isEqualTo("https");
+  }
+
+  @Test
+  void shouldGetProtocolForValue() {
+    assertThat(JhipsterApp.getProtocol("https")).isEqualTo("https");
+  }
+
+  @Test
+  void shouldGetContextPath() {
+    assertThat(JhipsterApp.getContextPath("/chips")).isEqualTo("/chips");
+  }
+
+  @Test
+  void shouldGetContextPathForNull() {
+    assertThat(JhipsterApp.getContextPath(null)).isEqualTo("/");
+  }
+
+  @Test
+  void shouldGetContextPathForBlank() {
+    assertThat(JhipsterApp.getContextPath(" ")).isEqualTo("/");
+  }
+
+  @Test
+  void shouldGetHost() {
+    assertThatCode(JhipsterApp::getHostAddress).doesNotThrowAnyException();
+  }
+
+  @Test
+  void shouldGetHostWithoutHostAddress() {
+    try (MockedStatic<InetAddress> inetAddress = Mockito.mockStatic(InetAddress.class)) {
+      inetAddress.when(InetAddress::getLocalHost).thenThrow(new UnknownHostException());
+
+      String result = JhipsterApp.getHostAddress();
+
+      assertThat(result).isEqualTo("localhost");
+    }
+  }
+}
